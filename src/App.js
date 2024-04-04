732353fd4baebda2446e09e86da657a19becc08f
Abdashimov Kubanychbek <abdashimov99@mail.ru>
2024-04-05 03:19:07 +0400

    have deleted the PizzaJson, then  added the useState for the pizzaItems in App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import PizzaCard from "./components/PizzaCard/PizzaCard";
-import pizzaJson from "./assets/pizzaJson.json"
-
-
-
-
+import React, {useEffect} from "react";
 
 function App() {
+    const [pizzaItems, setPizzaItems] = React.useState([])
+
+   useEffect(()=>{
+      fetch("https://660f2136356b87a55c50ddb1.mockapi.io/PizzaItems").then((item)=>{
+            return item.json();
+        }).then(response => setPizzaItems(response))
+   }, [])
+
+
     return (
         <div className="wrapper">
             <Header/>
@@ -17,7 +22,7 @@ function App() {
             <div className="content">
                 <h1>Все пиццы</h1>
                 <div className="pizza_items d-flex justify-around flex-wrap">
-                    {pizzaJson.map((pizza) =>(
+                    {pizzaItems.map((pizza) =>(
                         <PizzaCard
                             key={pizza.id}
                             {...pizza}
--- src/components/Navbar/SearchBtn.jsx
+++ src/components/Navbar/SearchBtn.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const SearchBtn = () => {
     const searchNames = ["по пулярности", "по цене", "по алфавиту"]
-    const [openSearch, setOpenSeacrh] = React.useState(false)
+    const [openSearch, setOpenSearch] = React.useState(false)
     const [chosenSearch, setChosenSearch] = React.useState(0)
 
     const onClickSelect =()=>{
-        setOpenSeacrh(prevState => !prevState)
+        setOpenSearch(prevState => !prevState)
     }
 
     const onClickChosen = (index) =>{
@@ -14,11 +14,11 @@ const SearchBtn = () => {
     }
     return (
         <div className="select_button" onClick={onClickSelect} >
-            <img src="/img/search_icon.svg" className={openSearch && "active_img"}/>
+            <img src="/img/search_icon.svg" className={openSearch ? "active_img" : ""}/>
             <button>Сортировка по: <span>{searchNames[chosenSearch]}</span></button>
             <ul style={{display: openSearch? "flex": "none"}}>
                 {searchNames.map((item, index)=>(
-                    <li onClick={()=>{onClickChosen(index)}} className= {chosenSearch === index && "chosenSearch"}>{item} </li>
+                    <li key={index} onClick={()=>{onClickChosen(index)}} className= {chosenSearch === index ? "chosenSearch" :""}>{item} </li>
                 ))}
             </ul>
         </div>
